68877acd2f1c21c07d6919bbd8810b309d619440
Calvin Feng <calvin.j.feng@gmail.com>
2023-07-12 19:13:26 -0700

    Implemented model checkpoint logic

--- train_ssd.py
+++ train_ssd.py
@@ -1,7 +1,11 @@
 import argparse
+import datetime
+import os
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
 
 from data.data_utils import collate_ground_truth_boxes
 from data.voc_dataset import VOCDataset
@@ -24,13 +28,16 @@ def train(args):
     prior_boxes = prior_boxes.to(device)
 
     encoder = TargetEncoder(prior_boxes, iou_threshold=0.5)
-    train_dataset = VOCDataset("train")
-    train_loader = DataLoader(train_dataset,
+    train_loader = DataLoader(VOCDataset("train"),
                               batch_size=args.batch_size,
                               num_workers=args.num_workers,
                               shuffle=True,
                               collate_fn=collate_ground_truth_boxes)
-
+    val_loader = DataLoader(VOCDataset("val"),
+                            batch_size=args.batch_size,
+                            num_workers=args.num_workers,
+                            shuffle=True,
+                            collate_fn=collate_ground_truth_boxes)
     multibox_loss = MultiBoxLoss()
     ssd = SingleShotDetector().to(device)
     optimizer = optim.SGD(ssd.parameters(),
@@ -38,21 +45,53 @@ def train(args):
                           momentum=args.momentum,
                           weight_decay=args.weight_decay)
 
-    for batch_img, batch_gt in train_loader:
-        batch_img = batch_img.to(device)
-        batch_gt = batch_gt.to(device)
+    summary_writer = None
+    if args.save_log:
+        current_time = datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+        summary_writer = SummaryWriter(log_dir=f"{args.log_dir}/ssd_{current_time}")
 
-        (batch_matched_gts,
-         batch_matched_labels,
-         batch_loc_targets,
-         batch_cls_targets) = encoder.encode_batch(batch_gt[:, :, :4], batch_gt[:, :, 4])
+    global_step = 0
+    epoch = 0
+    if args.load_checkpoint:
+        checkpoint = torch.load(args.load_checkpoint)  # Load a checkpoint after 10 epochs
+        ssd.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        global_step = checkpoint['global_step']
+        epoch = checkpoint['epoch']
 
-        optimizer.zero_grad()
-        loc_preds, cls_preds = ssd(batch_img)
-        loss = multibox_loss(loc_preds, cls_preds, batch_loc_targets, batch_cls_targets)
-        print(loss.item())
-        loss.backward()
-        optimizer.step()
+    for _ in range(args.num_epochs):
+        for batch_img, batch_gt in tqdm(train_loader):
+            batch_img = batch_img.to(device)
+            batch_gt = batch_gt.to(device)
+
+            (batch_matched_gts,
+            batch_matched_labels,
+            batch_loc_targets,
+            batch_cls_targets) = encoder.encode_batch(batch_gt[:, :, :4], batch_gt[:, :, 4])
+
+            optimizer.zero_grad()
+            loc_preds, cls_preds = ssd(batch_img)
+            loss = multibox_loss(loc_preds, cls_preds, batch_loc_targets, batch_cls_targets)
+            if summary_writer:
+                summary_writer.add_scalar("loss/train", loss, global_step)
+            loss.backward()
+            optimizer.step()
+            global_step += 1
+            if global_step % 10 == 0:
+                break
+
+        epoch += 1
+        if epoch % args.checkpoint_interval == 0:
+            os.makedirs(args.checkpoint_folder, exist_ok=True)
+            torch.save({
+                'epoch': epoch,
+                'global_step': global_step,
+                'model_state_dict': ssd.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+            }, f"{args.checkpoint_folder}/ssd_epoch_{epoch}.pth")
+
+    if args.save_log:
+        summary_writer.close()
 
 
 args = argparse.ArgumentParser(description="Single Shot Detector Training With PyTorch")
@@ -62,9 +101,12 @@ args.add_argument("--momentum", default=0.9, type=float, help="Momentum value fo
 args.add_argument("--weight_decay", default=5e-4, type=float, help="Weight decay for SGD")
 args.add_argument("--num_epochs", default=100, type=int, help="Number of epochs to train for")
 args.add_argument("--num_workers", default=1, type=int, help="Number of workers used in dataloading")
-args.add_argument("--checkpoint_folder", default="ssd_checkpoints", type=str, help="Directory for saving checkpoint models")
+
+args.add_argument("--load_checkpoint", default=None, type=str, help="Path to model checkpoint")
+args.add_argument("--checkpoint_folder", default="checkpoints", type=str, help="Directory for saving checkpoint models")
 args.add_argument("--checkpoint_interval", default=10, type=int, help="Number of epochs between saving checkpoints")
-args.add_argument("--tensorboard", default=True, type=bool, help="Use tensorboard for loss visualization")
+
+args.add_argument("--save_log", action='store_true', help="Use tensorboard for loss visualization")
 args.add_argument("--log_dir", default="logs", type=str, help="Directory for tensorboard logs")
 
 
